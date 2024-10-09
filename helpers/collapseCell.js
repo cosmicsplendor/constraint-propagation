d51b3a68150c126dda4c36b1eb65d95e8ff26a0d
Umesh KC <ufocalypse@gmail.com>
2024-10-09 15:49:47 +0545

    Add collapseCell helper for collapsing a cell superposition into a concrete value based on weighted probability

---
+++ helpers/collapseCell.js
@@ -0,0 +1,20 @@
+function collapseCell(grid, row, col) {
+    const possibleTiles = grid[row][col];
+    const chosenTile = chooseTileWithWeight(possibleTiles);
+    grid[row][col] = [chosenTile]; // Collapse to one tile
+}
+
+function chooseTileWithWeight(possibleTiles) {
+    let rand = Math.random();  // Since the weights sum to 1, we can directly use this random value
+    
+    for (let i = 0; i < possibleTiles.length; i++) {
+        rand -= possibleTiles[i].weight;  // Subtract the weight of the current tile
+        if (rand <= 0) {
+            return possibleTiles[i].tile;  // Return the tile when the cumulative weight exceeds the random value
+        }
+    }
+
+    // Fallback in case of rounding issues (shouldn't normally happen since weights sum to 1)
+    return possibleTiles[possibleTiles.length - 1].tile;
+}
+module.exports = collapseCell
