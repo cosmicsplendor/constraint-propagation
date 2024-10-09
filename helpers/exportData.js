65256a1258d7cf048ede9b478ed451fea094a235
Umesh KC <ufocalypse@gmail.com>
2024-10-09 18:48:53 +0545

    Sanitize and export generated grid data

---
+++ helpers/exportData.js
@@ -0,0 +1,14 @@
+const fs = require("fs")
+const sanitizeGrid = grid => {
+    return grid.map(row => {
+        return row.map(cell => {
+            if (cell.length === 0) return "empty"
+            return typeof cell[0] === "string" ? cell[0]: cell[0].tile
+        })
+    })
+}
+const exportData = (path, grid) => {
+    fs.writeFileSync(path, JSON.stringify(sanitizeGrid(grid)))
+}
+
+module.exports = exportData
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const execWFC = require("./helpers/execWFC");
-const table = require("./adjacencyTable.json")
+const table = require("./adjacencyTable.json");
+const exportData = require("./helpers/exportData");
 const grid = execWFC(table, 4, 4)
+exportData("./test.json", grid)
